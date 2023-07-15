38c9b47cc22c7346ecc17b5a0a16b07553b48507
Abhish Khanal <akhanal7@gmu.edu>
2023-07-14 21:08:00 -0400

    Add vim, vscode, ssh and git

---
+++ link.sh
@@ -0,0 +1,6 @@
+ln -s "$(pwd)/ssh/config" ~/.ssh/config
+ln -s "$(pwd)/vim/vimrc" ~/.vimrc
+ln -s "$(pwd)/git/gitconfig" ~/.gitconfig
+ln -s "$(pwd)/vscode/settings.json" ~/.config/Code/User/settings.json
+ln -s "$(pwd)/vscode/keybindings.json" ~/.config/Code/User/keybindings.json
+
