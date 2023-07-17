64a4164ea9747022095465ae46fd0bf9d6de1ae0
Abhish Khanal <abheeshkhanal@gmail.com>
2023-07-17 10:40:41 -0400

    add tmux configuration

--- link.sh
+++ link.sh
@@ -1,9 +1,10 @@
 # Delete existing files before linking
 rm -rf ~/.ssh/config ~/.vimrc ~/.gitconfig ~/.config/Code/User/settings.json ~/.config/Code/User/keybindings.json
+rm -rf ~/.tmux.conf
 
 ln -s "$(pwd)/ssh/config" ~/.ssh/config
 ln -s "$(pwd)/vim/vimrc" ~/.vimrc
 ln -s "$(pwd)/git/gitconfig" ~/.gitconfig
 ln -s "$(pwd)/vscode/settings.json" ~/.config/Code/User/settings.json
 ln -s "$(pwd)/vscode/keybindings.json" ~/.config/Code/User/keybindings.json
-
+ln -s "$(pwd)/tmux/tmux.conf" ~/.tmux.conf
