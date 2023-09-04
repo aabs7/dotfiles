e4415eb5e8fe85de05e95c725d44ade2cee92e69
Abhish Khanal <abheeshkhanal@gmail.com>
2023-09-04 15:30:02 -0400

    capture add, refile add

--- emacs.d/config/org.el
+++ emacs.d/config/org.el
@@ -14,8 +14,7 @@
   (setq org-log-done 'time) ;; log time when task is done
   (setq org-log-into-drawer t) ;; time stamps into drawer 
   (setq org-agenda-files
-	'("~/orgfiles/daily_logs/Task.org"
-	  "~/orgfiles/Birthday.org")))
+	'("~/orgfiles/")))
 
 (use-package org-roam
   :ensure t
@@ -38,10 +37,41 @@
   :config
   (org-roam-setup))
 
-
 (setq org-todo-keywords
-    '((sequence "TODO(t)" "REMAINING(n)" "|" "DONE(d!)")))
+      (quote ((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)")
+              (sequence "WAITING(w@/!)" "HOLD(h@/!)" "|" "CANCELLED(c@/!)" "PHONE" "MEETING"))))
 
+(setq org-todo-keyword-faces
+      (quote (("TODO" :foreground "red" :weight bold)
+              ("NEXT" :foreground "light blue" :weight bold)
+              ("DONE" :foreground "forest green" :weight bold)
+              ("WAITING" :foreground "orange" :weight bold)
+              ("HOLD" :foreground "magenta" :weight bold)
+              ("CANCELLED" :foreground "forest green" :weight bold)
+              ("MEETING" :foreground "forest green" :weight bold)
+              ("PHONE" :foreground "forest green" :weight bold))))
+
+(setq org-directory "~/orgfiles")
+(setq org-default-notes-file "~/orgfiles/refile.org")
+;; I use C-c c to start capture mode
+(global-set-key (kbd "C-c c") 'org-capture)
+
+;; Capture templates for: TODO tasks, Notes, appointments, phone calls, meetings, and org-protocol
+(setq org-capture-templates
+     (quote (("t" "todo" entry (file "~/orgfiles/refile.org")
+               "* TODO %?\n%U\n%a\n" :clock-in t :clock-resume t)
+              ("r" "respond" entry (file "~/orgfiles/refile.org")
+               "* NEXT Respond to %:from on %:subject\nSCHEDULED: %t\n%U\n%a\n" :clock-in t :clock-resume t :immediate-finish t)
+              ("n" "note" entry (file "~/orgfiles/refile.org")
+               "* %? :NOTE:\n%U\n%a\n" :clock-in t :clock-resume t)
+              ("w" "org-protocol" entry (file "~/orgfiles/refile.org")
+               "* TODO Review %c\n%U\n" :immediate-finish t)
+              ("m" "Meeting" entry (file "~/orgfiles/refile.org")
+               "* MEETING with %? :MEETING:\n%U" :clock-in t :clock-resume t)
+              )))
+
+(setq org-refile-targets (quote ((nil :maxlevel . 9)
+                                 (org-agenda-files :maxlevel . 9))))
 
 ;; fill left and right part so that text is in the middle
 (defun efs/org-mode-visual-fill ()
