1eb18ed656ace30264774cd1d51484f1b7df16be
Abhish Khanal <abheeshkhanal@gmail.com>
2023-09-04 15:22:18 -0400

    Full screen and magit add

--- emacs.d/config/general.el
+++ emacs.d/config/general.el
@@ -12,18 +12,23 @@
 
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit) ; Make ESC quit prompts
 
-(define-key global-map "\C-cc" 'org-capture)
-
+;; don't show line numbers for the following modes
 (dolist (mode '(org-mode-hook
 		term-mode-hook
 		shell-mode-hook
 		eshell-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
-
+(add-to-list 'default-frame-alist '(fullscreen . maximized)) ;; full screen mode on startup
+(add-hook 'window-setup-hook 'toggle-frame-fullscreen) ;; toggle the extended full screen mode after emacs has started
+;; This package shows which key does what after some delay
 (use-package which-key
   :defer 0
   :diminish which-key-mode
   :config
   (which-key-mode)
   (setq which-key-idle-delay 0))
+
+;; Magit package for git
+(use-package magit
+  :ensure t)
